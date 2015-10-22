f65247f6a3acd48c0595c2f964bcc77656a6fee0
yejing <mailfendou@163.com>
2015-10-22 19:23:35 +0800

    增加了一个后置处理器类

--- src/com/maven/flow/editor/extend/interfaces/ClassNameGetter.java
+++ src/com/maven/flow/editor/extend/interfaces/ClassNameGetter.java
@@ -6,51 +6,43 @@ import com.inbuild.audit.wordflow.enger.application.impl.*;
 import com.maven.flow.util.ClassUtil;
 
 public class ClassNameGetter {
-	
-	public static String[] getFlowPathsClass(){
-		String s[]={
-					
-					NeedCarFlowPath.class.getName(),
-					NotNeedCarFlowPath.class.getName(),
-					OverFiveMillion.class.getName(),
-					ReCheckerManagerFlowPath.class.getName(),
-					SmallerThanFiveMillion.class.getName(),
-					AuditToReAuditFlowPath.class.getName(),
-					CheckDepartMeneToReCheckDepartMentFlowPath.class.getName(),
-					OverAndSmallerThanFiveMoney.class.getName(),
-					OverThanTwoHandMoney.class.getName(),
-					SmallerThanFiveMoney.class.getName(),
-					BigThanFiveMoney.class.getName(),
-					OnlyCenterShenHeFinish.class.getName()
-		};
+
+	public static String[] getFlowPathsClass() {
+		String s[] = { NeedCarFlowPath.class.getName(),
+				NotNeedCarFlowPath.class.getName(),
+				OverFiveMillion.class.getName(),
+				ReCheckerManagerFlowPath.class.getName(),
+				SmallerThanFiveMillion.class.getName(),
+				AuditToReAuditFlowPath.class.getName(),
+				CheckDepartMeneToReCheckDepartMentFlowPath.class.getName(),
+				OverAndSmallerThanFiveMoney.class.getName(),
+				OverThanTwoHandMoney.class.getName(),
+				SmallerThanFiveMoney.class.getName(),
+				BigThanFiveMoney.class.getName(),
+				OnlyCenterShenHeFinish.class.getName() };
 		return s;
 	}
-	
-	public static String[] getFinishCondiction(){
-		String s[]={
-				FinishedNoCondiction.class.getName(),
-				OnlyPinShenZhongXinFinish.class.getName()
-		};
+
+	public static String[] getFinishCondiction() {
+		String s[] = { FinishedNoCondiction.class.getName(),
+				OnlyPinShenZhongXinFinish.class.getName() };
 		return s;
 	}
-	
-	
-	public static void main(String args[]){
-		Set set=ClassUtil.getClasses(Package.getPackage("com.maven.flow.editor.extend.impl"));
-		if(set!=null){
-			Object [] o=set.toArray();
-			for(int i=0;i<o.length;i++){
-				Class c=(Class)o[i];
-				
-				
+
+	public static void main(String args[]) {
+		Set set = ClassUtil.getClasses(Package
+				.getPackage("com.maven.flow.editor.extend.impl"));
+		if (set != null) {
+			Object[] o = set.toArray();
+			for (int i = 0; i < o.length; i++) {
+				Class c = (Class) o[i];
+				System.out.println(c.getCanonicalName());
 			}
 		}
 	}
-	
-	
-	public static String[] getBeforeHandleClass(){
-		String s[]={
-				BeforeNoUse.class.getName(),
+
+	public static String[] getBeforeHandleClass() {
+		String s[] = { BeforeNoUse.class.getName(),
 				BeforeCheck.class.getName(),
 				BeforeFuShenKeFuShen.class.getName(),
 				BeforeFuZhuRenShenPiPingShenJieLun.class.getName(),
@@ -61,19 +53,12 @@ public class ClassNameGetter {
 				BeforeSplitPerson.class.getName(),
 				BeforeTiJiaoPingShenJieLun.class.getName(),
 				BeforeXiuZhengBaoGao.class.getName(),
-				BeforeEnd.class.getName(),
-				BeforeNotifySendEnd.class.getName()
-		};
-		
-		
-		
+				BeforeEnd.class.getName(), BeforeNotifySendEnd.class.getName() };
 		return s;
 	}
-	
-	public static String[] getAfterHandleClass(){
-		String s[]={
-				AfterNotUse.class.getName(),
-				AfterCheck.class.getName(),
+
+	public static String[] getAfterHandleClass() {
+		String s[] = { AfterNotUse.class.getName(), AfterCheck.class.getName(),
 				AfterCheckManagerSubmit.class.getName(),
 				AfterFuLingDaoTiJiaoPingShenJieLun.class.getName(),
 				AfterGuiDang.class.getName(),
@@ -82,10 +67,9 @@ public class ClassNameGetter {
 				AfterReReCheckManagerSubmit.class.getName(),
 				AfterSetReport.class.getName(),
 				AfterTiJianPianShengJieLun.class.getName(),
-				AfterZhengLingDaoTiJiaoPingShenJieLun.class.getName()
-		};
-		
+				AfterZhengLingDaoTiJiaoPingShenJieLun.class.getName(),
+				AfterShouLi.class.getName() };
 		return s;
 	}
-	
+
 }
